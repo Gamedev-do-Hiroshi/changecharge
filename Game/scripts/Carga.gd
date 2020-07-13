83b73fc8190eb0759a32d92827ef747dc004710d
LeaoLuciano <lucianorsleao@gmail.com>
2020-07-12 21:58:05 -0300

    Uma primeira perspectiva do jogo

--- Game/scripts/Carga.gd
+++ Game/scripts/Carga.gd
@@ -1,30 +1,29 @@
 extends Area2D
+
+onready var raiz = self.get_parent()
+
 var sinal = true
 var mouse_entrou = false
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var charge = 10
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.animation = "positive"
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	for no in raiz.get_children():
+		if no.get_groups().has("player"):
+			no.cargas.push_back(self)
+		pass
 
 func _input(event):
+	
 	if mouse_entrou and event.is_pressed() and event.button_index == BUTTON_LEFT:
 		print("CLICKED")
 		sinal = !sinal
+		charge = -charge
 		$Sprite.animation = "positive" if sinal else "negative"
 	
 func _on_Carga_mouse_entered():
 	mouse_entrou = true
-	pass # Replace with function body.
 
 
 func _on_Carga_mouse_exited():
 	mouse_entrou = false
-	pass # Replace with function body.
--- Game/scripts/Player.gd
+++ Game/scripts/Player.gd
@@ -1,4 +1,31 @@
 extends KinematicBody2D
 
+onready var raiz = self.get_parent()
+
+var cargas = []
+
+var vel = Vector2()
+var ace = Vector2()
+
+var K = 1000.0
+var aux
+export var charge = 10.0
+
 func _ready():
+	
 	pass
+
+func _physics_process(delta):
+	
+	ace = Vector2()
+	for carga in cargas:
+		aux = (self.position - carga.position).length()
+		ace += K * (self.position - carga.position).normalized() * carga.charge * charge / (aux * aux)
+	
+	vel += ace * delta
+	
+	self.position += vel * delta
+	
+	
+	
+	
