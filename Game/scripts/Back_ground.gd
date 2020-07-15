3a48175358f11d6971fc454e45ed4fb1ee9fe409
@rafaelgtvc <rafaelgtvc@gmail.com>
2020-07-15 01:23:45 -0300

    Menu quase pronto

--- Game/scripts/Back_ground.gd
+++ Game/scripts/Back_ground.gd
@@ -1,13 +1,37 @@
-extends Node
+extends Node2D
 
 onready var X = preload("res://scenes/X.tscn")
 
+var L = 10
+var t = 0
+var width = 128
+var height = 128
+var r = 0
+var g = 0
+var b = 0
+var h = float(height)
+var w = float(width)
+
 func _ready():
-	
-	
+	set_process(true)
 	for i in 20:
 		for j in 12:
 			if i < 5 || i > 14:
 				var s = X.instance()
 				s.position = Vector2(16 + i*64, 16 + j*64)
 				add_child(s)
+
+func _process(delta):
+	update()
+	t+=delta
+
+func _draw():
+	for x in range(width):
+		for y in range(height):
+			
+			r = .5*x/w*(sin(t)+PI/2)
+			b = .5*(w-x)/w*(cos(t)+PI/2)
+			
+			
+			draw_rect(Rect2(Vector2(x*L,y*L),Vector2(L,L)),Color(r,g,b))
+
--- Game/scripts/Title_Screen.gd
+++ Game/scripts/Title_Screen.gd
@@ -1,9 +1,13 @@
 extends Node
+ 
+var time = Timer 
 
 
 func _physics_process(delta):
 	$AnimationPlayer.play("twinkle")
-
+	if $TextureButton.is_pressed():
+		$AudioStreamPlayer2.play()
+		$AnimationPlayer.play("select")
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://scenes/chooseLevel.tscn")
