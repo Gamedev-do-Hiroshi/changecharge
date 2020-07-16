07fb77cca33b6c7fcfefb0096f57574d0b159c4c
LeaoLuciano <lucianorsleao@gmail.com>
2020-07-15 21:33:47 -0300

    Trapezio corrigido :)

--- Game/scripts/Player.gd
+++ Game/scripts/Player.gd
@@ -6,7 +6,9 @@ var cargas = []
 
 var vel = Vector2()
 var ace = Vector2()
+var dace = Vector2()
 var ant_ace = Vector2()
+var ant_dace = Vector2()
 
 var vel_pot = 0.0
 var K = 1000.0
@@ -29,21 +31,26 @@ func _physics_process(delta):
 func movimento(delta):
 	
 	ant_ace = ace
+	ant_dace = dace
 	ace = Vector2()
+	dace = Vector2()
 	
 	for carga in cargas:
 		aux = (self.position - carga.position).length()
 		ace += K * (self.position - carga.position).normalized() * carga.charge * charge / (aux * aux)
+		dace -= 2*K * (self.position - carga.position).normalized() * carga.charge * charge / (aux * aux * aux)
 	
 	ace += charge * campo_magnetico * vel.tangent()
+	dace += charge * campo_magnetico * ace.tangent()
 	
 	
 	if primeira_passada:
 		ant_ace = ace
+		ant_dace = dace
 		primeira_passada = false
 	
 	
-	vel += (ace + ant_ace ) * delta / 2
+	vel += (ace + ant_ace ) * delta / 2 + vel * (dace - dace) * delta * delta / 12
 	
 	#self.position += vel * delta
 	
