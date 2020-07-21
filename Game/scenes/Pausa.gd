5015601b070a8f5bfc28072d2846b09575b0d44a
@rafaelgtvc <rafaelgtvc@gmail.com>
2020-07-21 15:38:23 -0300

    Menu instanciado

--- Game/scenes/Pausa.gd
+++ Game/scenes/Pausa.gd
@@ -1,6 +1,12 @@
 extends Control
 
 
+var menu = preload("res://scenes/Menu.tscn")
+
+func _ready():
+	var child = menu.instance()
+	add_child(child)
+	
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_state = not get_tree().paused
