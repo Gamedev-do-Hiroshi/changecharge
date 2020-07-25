49557739e8a1de2168a7ac36d07aea96f769dcf2
LeaoLuciano <lucianorsleao@gmail.com>
2020-07-25 19:25:02 -0300

    Fase 14 terminada?

--- Game/scripts/Fases/Fase_14.gd
+++ Game/scripts/Fases/Fase_14.gd
@@ -1,4 +1,23 @@
 extends Node2D
 
+const v_raio = 3.0
+const L = 150.0
+
+
+onready var raio1 = $Obstaculos/Pivo/Cerca_Eletrica4
+
+var ini_raio1
+
+var t = 0.0
+
 func _ready():
-	pass
+	ini_raio1 = raio1.position.y
+	
+	t = 0.0
+
+func _physics_process(delta):
+	t += delta
+	raio1.position.y = ini_raio1 + max(L/2*sin(v_raio*t), 0) - L/2
+	
+	raio1.scale.y = 0.7*max(sin(v_raio*t), 0)
+		
